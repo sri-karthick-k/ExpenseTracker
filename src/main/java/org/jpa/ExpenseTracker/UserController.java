9ce621b64da9805612c46883fd1bb0e971f4c4f9
sri-karthick-k <srikarthickk.mca22@rvce.edu.in>
2024-02-16 12:34:35 +0530

    add controllers for register and login

--- src/main/java/org/jpa/ExpenseTracker/UserController.java
+++ src/main/java/org/jpa/ExpenseTracker/UserController.java
@@ -1,2 +1,29 @@
-package org.jpa.ExpenseTracker;public class UserController {
+package org.jpa.ExpenseTracker;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/user")
+public class UserController {
+    private final ETService ets;
+
+    @Autowired
+    public UserController(ETService ets){
+        this.ets = ets;
+    }
+
+    @PostMapping ("/register")
+    public int register(@RequestBody User request) {
+        return ets.register(request.getName(), request.getEmail(), request.getPassword());
+    }
+
+    @PostMapping("/login")
+    public int login(@RequestBody User request) {
+        return ets.login(request.getEmail(), request.getPassword());
+    }
+
 }
