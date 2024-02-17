b505ff7eedbb949f056a653816320616fe31a955
sri-karthick-k <srikarthickk.mca22@rvce.edu.in>
2024-02-17 11:06:05 +0530

    add method with Query to fetch all the categories of a user

--- src/main/java/org/jpa/ExpenseTracker/Category/CategoryRepo.java
+++ src/main/java/org/jpa/ExpenseTracker/Category/CategoryRepo.java
@@ -1,2 +1,11 @@
-package org.jpa.ExpenseTracker.Category;public class CategoryRepo {
+package org.jpa.ExpenseTracker.Category;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
+
+public interface CategoryRepo extends JpaRepository<Category, Integer> {
+    @Query("SELECT new Category(c.cID, c.name) FROM Category c WHERE c.user.id = ?1")
+    List<Category> findCategoryByUserId(int u_id);
 }
