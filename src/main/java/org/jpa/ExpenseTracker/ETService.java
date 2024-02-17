d1a17c92bdbfe816d325800945e9de33140f5b8e
sri-karthick-k <srikarthickk.mca22@rvce.edu.in>
2024-02-17 11:07:18 +0530

    add methods to fetch and create values in category

--- src/main/java/org/jpa/ExpenseTracker/ETService.java
+++ src/main/java/org/jpa/ExpenseTracker/ETService.java
@@ -1,5 +1,8 @@
 package org.jpa.ExpenseTracker;
 
+import org.jpa.ExpenseTracker.Category.*;
+import org.jpa.ExpenseTracker.User.User;
+import org.jpa.ExpenseTracker.User.UserRepo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -9,6 +12,14 @@ import java.util.List;
 public class ETService {
     @Autowired
     UserRepo ur;
+//    CategoryRepo cr;
+
+    private final CategoryRepo cr;
+
+    @Autowired
+    public ETService(CategoryRepo cr) {
+        this.cr = cr;
+    }
 
     public int register(String name, String email, String password){
         List<String> users = ur.findByEmail(email);
@@ -26,6 +37,8 @@ public class ETService {
         if(!user.isEmpty()){
             List<String> matchedNames = ur.findByEmailAndPassword(email, password);
             if(!matchedNames.isEmpty()){
+                // return username
+                System.out.println(user.get(0));
                 // Valid credentials
                 return 1;
             } else {
@@ -42,13 +55,33 @@ public class ETService {
     public List<User> displayAll(){
         List<User> users = ur.findAll();
         for(User u : users){
-            System.out.println(u.name);
-            System.out.println(u.email);
-            System.out.println(u.password);
+            System.out.println(u.getName());
+            System.out.println(u.getEmail());
+            System.out.println(u.getPassword());
         }
         return users;
     }
 
+    public String addCategory(int u_id, String c_name){
+        User user = ur.findById(u_id).orElse(null);
+        if(user != null){
+            User u = new User();
+            u.setId(u_id);
+            Category newCategory = new Category(c_name, u);
+            cr.save(newCategory);
+            return "SUCCESS";
+        } else {
+            return "USER NOT FOUND";
+        }
+    }
+
+    public List<Category> getCategoryUser(int u_id){
+        User user = ur.findById(u_id).orElse(null);
+        if(user != null){
+            return cr.findCategoryByUserId(u_id);
+        }
+        return null;
+    }
 
 
 }
