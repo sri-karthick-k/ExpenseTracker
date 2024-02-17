25b27376c9b602bda705d3897173f79077e85290
sri-karthick-k <srikarthickk.mca22@rvce.edu.in>
2024-02-17 11:06:45 +0530

    init get and post handlers to fetch and create values in category

--- src/main/java/org/jpa/ExpenseTracker/Category/CategoryController.java
+++ src/main/java/org/jpa/ExpenseTracker/Category/CategoryController.java
@@ -1,2 +1,34 @@
-package org.jpa.ExpenseTracker.Category;public class CategoryController {
+package org.jpa.ExpenseTracker.Category;
+
+import org.jpa.ExpenseTracker.ETService;
+import org.jpa.ExpenseTracker.User.User;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Map;
+
+@RestController
+@RequestMapping("/api/v1/category")
+public class CategoryController {
+    private final ETService ets;
+
+    @Autowired
+    public CategoryController(ETService ets){
+        this.ets = ets;
+    }
+
+    @GetMapping("/{u_id}")
+    public List<Category> getUserCategory(@PathVariable int u_id){
+        return ets.getCategoryUser(u_id);
+    }
+
+    @PostMapping("/new")
+    public String addNewCategory(@RequestBody Map<String, Object> payload) {
+        int uid = (int) payload.get("uid");
+        String category_name = (String) payload.get("category_name");
+        return ets.addCategory(uid, category_name);
+    }
+
+
 }
