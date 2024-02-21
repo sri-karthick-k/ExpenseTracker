e23228677df7553dcdb5fee23f2e45875f1b106e
sri-karthick-k <srikarthickk.mca22@rvce.edu.in>
2024-02-21 11:54:18 +0530

    add error handling route

--- src/main/java/org/jpa/ExpenseTracker/User/UserViewController.java
+++ src/main/java/org/jpa/ExpenseTracker/User/UserViewController.java
@@ -1,7 +1,5 @@
 package org.jpa.ExpenseTracker.User;
 
-import org.jpa.ExpenseTracker.ETService;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
@@ -10,12 +8,6 @@ import org.springframework.web.bind.annotation.*;
 @Controller
 @RequestMapping("/user")
 public class UserViewController {
-    private final ETService ets;
-
-    @Autowired
-    public UserViewController(ETService ets){
-        this.ets = ets;
-    }
 
     @GetMapping("/")
     public String index() {
@@ -26,31 +18,20 @@ public class UserViewController {
         return "user/register";
     }
 
-    @PostMapping("/register")
-    public String register(@RequestBody User request) {
-        int status = ets.register(request.getName(), request.getEmail(), request.getPassword());
-        if(status == 1)
-            return "redirect:/login";
-        else
-            return "redirect:/register?error=true";
-    }
-
     @GetMapping("/login")
     public String loginForm() {
         return "user/login";
-    }
-
-    @PostMapping("/login")
-    public String login(@RequestBody User request) {
-        int status = ets.login(request.getEmail(), request.getPassword());
-        if(status == 1)
-            return "redirect:/dashboard"; // Redirect to dashboard upon successful login
-        else
-            return "redirect:/login?error=true"; // Redirect to login page with error flag
     }
 
     @GetMapping("/dashboard")
     public String dashboard() {
         return "index";
     }
+
+    @GetMapping("/error")
+    public String loginError(){
+        return "user/login-error";
+    }
+
+
 }
